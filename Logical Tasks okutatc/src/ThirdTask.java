99c08f9943d9feb4d1a39da7bffeb692223fab48
OlexandrKutaiev <p4422663331114@gmail.com>
2017-09-23 23:26:08 +0300

    redesigned third task algorithm

--- Logical Tasks okutatc/src/ThirdTask.java
+++ Logical Tasks okutatc/src/ThirdTask.java
@@ -14,15 +14,17 @@ public class ThirdTask
 	public void GetResult()
 	{
 		System.out.print("Mersen's numbers is: ");
-		
-		//looking candidates on line [2 .. n)
-		for(int candidate = 2; candidate < this.n; candidate++)
-		{			
-			if(!IsPrime(candidate)) // whether mersen-candidate is prime
-				continue;
-			
-			if (IsMersen(candidate)) //whether mersen-candidate is truly Mersen
-				System.out.print(" " + candidate);
+		int candidate = 0;
+		for(int p = 2; ; p++)
+		{
+			if(IsPrime(p))
+			{
+				candidate = (int)(Math.pow(2, p) - 1);
+				if (candidate >= this.n)
+					break;
+				if (IsPrime(candidate))
+					System.out.print(" " + candidate);
+			}
 		}
 	}
 	
@@ -43,19 +45,4 @@ public class ThirdTask
 		}
 		return true;
 	}
-	
-	private boolean IsMersen(int number)
-	{
-		int p = 1; // (first prime number) - 1
-		while((int)(Math.pow(2, p) - 1) <= number)
-		{
-			p++;
-			if(!IsPrime(p)) //whether index p prime
-				continue;
-			
-			if ((int)(Math.pow(2, p) - 1) == number)
-				return true;
-		}
-		return false;
-	}	
 }
