d3a2e9572f8f42ced4e8282996d80de78bf12e91
SerhiienkoUliana <gugik2006@gmail.com>
2017-09-24 22:41:58 +0300

    homework 3 tasks

--- task2/src/main/java/Task331.java
+++ task2/src/main/java/Task331.java
@@ -1,6 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Task331 {
     /*Дано натуральное число n. Можно ли представить его в виде суммы трех квадратов натуральных
     чисел? Если можно, то указать тройку x, y, z таких натуральных чисел,
     что n = x^2 + y^2 + z^2.*/
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the natural number. Enter 'q' for the exit");
+        while (true) {
+            String s = reader.readLine();
+            if (s.equals("q")) break;
+            int n = Integer.parseInt(s);
+
+            int count = 0;
+
+            for (int x = 1; x <= n; x++) {
+                for (int y = 1; y <= x; y++) {
+                    for (int z = 1; z <= y; z++) {
+                        if (x * x + y * y + z * z ==n) {
+                            System.out.println(x + " " + y + " " + z);
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            if (count == 0) System.out.println("It is no possible.");
+
+        }
+    }
 }
--- task2/src/main/java/Task554.java
+++ task2/src/main/java/Task554.java
@@ -1,6 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Task554 {
     /*Дано натуральное число n. Получить все пифагоровы тройки натуральных чисел, каждое из которых
     не превосходит n, т.е. все такие тройки натуральных чисел a, b, c, что
     а^2 + b^2 = с^2 (а<=b<=c<=n).*/
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the natural number. Enter 'q' for the exit");
+        while (true) {
+            String s = reader.readLine();
+            if (s.equals("q")) break;
+            int n = Integer.parseInt(s);
+
+            int count = 0;
+            for (int a = 1; a <= n; a++) {
+                for (int b = 1; b <= n; b++) {
+                    for (int c = 1; c <= n; c++) {
+                        if (a * a + b * b == c * c) {
+                            System.out.println(a + " " + b + " " + c);
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            if (count == 0) System.out.println("It is no possible.");
+        }
+    }
 }
