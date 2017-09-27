2160b0d155f879194d7a6b6540deba62b25cd75f
MykolaShevchuk <shevchuk.mykola93@gmail.com>
2017-09-27 14:00:40 +0300

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,8 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-    Application application=new Application();
-    application.work();
+        Application application = new Application();
+        application.work();
 
     }
 }
