26069ee2f3fb6abf1b2d98a5e119b2b344575a9f
MykolaShevchuk <shevchuk.mykola93@gmail.com>
2017-09-27 22:20:05 +0300

    add gitignore

--- src/main/java/Models/TaskModel.java
+++
@@ -1,9 +0,0 @@
-package Models;
-
-import java.util.ArrayList;
-
-public abstract class TaskModel {
-    public ArrayList<Integer> numberOperation(Integer naturalNumber) {
-        return null;
-    }
-}
