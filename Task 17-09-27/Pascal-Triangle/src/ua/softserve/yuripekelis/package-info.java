2077e727aa64601f00baf2cf7cd9635d14b6c044
YuriPekelis <pekelis@gmail.com>
2017-09-29 00:27:06 +0300

    Change scanner class, delete redundant comments, get less quantity of classes, add InputStrem parameter to the scanner

--- Task 17-09-27/Pascal-Triangle/src/ua/softserve/yuripekelis/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package consist of classes, which solve the task of
- * printing preset number Pascal triangle's rows.
- */
-package ua.softserve.yuripekelis;
--- Task 17-09-27/min_multiple/src/ua/softserve/yuripekelis/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package consist of classes, which solve the task of counting the minimum
- * multiply for 2 numbers.
- */
-package ua.softserve.yuripekelis;
