8d1c7b3ffb17d11f09b0b05dc28c4137274349a0
AKravtsov <andrjusyk@gmail.com>
2017-09-29 01:03:36 +0300

    checkstyles2

--- src/com/softserve/edu/exercise1/Work1.java
+++ src/com/softserve/edu/exercise1/Work1.java
@@ -1,13 +1,22 @@
 package com.softserve.edu.exercise1;
-
+/**
+ * Class for know quantity and sum.
+ * @author Andrii
+ *
+ */
 public class Work1 {
 	public int quantity;
 	public int sum;
-
-	public void outwork(int[] mas) {
+	private final int seven = 7;
+	private final int five = 5;
+	/**
+	 * Method work with integers and enumerate quan and sum.
+	 * @param mas massive with numbers
+	 */
+	public void outwork(final int[] mas) {
 		int i = 0;
 		while (i < mas.length) {
-			if (mas[i] % 5 == 0 && mas[i] % 7 != 0) {
+			if ((mas[i] % five == 0) && (mas[i] % seven != 0)) {
 				quantity++;
 				sum += mas[i];
 			}
