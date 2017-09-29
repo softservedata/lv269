b980b0030875142ec9389002488d43aeba6c1f44
MykolaShevchuk <shevchuk.mykola93@gmail.com>
2017-09-29 11:53:24 +0300

    Revert "comments & etc."
    This reverts commit 6f578cc60bb19006018587b6748450610860d799.

--- src/main/java/Models/TaskModel.java
+++ src/main/java/Models/TaskModel.java
@@ -2,9 +2,6 @@ package Models;
 
 import java.util.ArrayList;
 
-/**
- * its model for all tasks.
- */
 public abstract class TaskModel {
     public ArrayList<Integer> numberOperation(Integer naturalNumber) {
         return null;
--- src/main/java/Models/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Here ir programing realisation jf tasks, which are represented in readme.txt
- *
- */
-package Models;
