2efff64dc8259afcfb6b21d075f04418aabc0ecb
MykolaShevchuk <shevchuk.mykola93@gmail.com>
2017-09-29 12:23:48 +0300

    final

--- src/main/java/Models/Factorial.java
+++ src/main/java/Models/Factorial.java
@@ -1,7 +1,14 @@
 package Models;
 
+/**
+ * class made for counting Factorial
+ */
 public class Factorial {
-
+    /**
+     *
+     * @param number
+     * @return  (n-1)!
+     */
     public static Integer factorialFromNMinusOne(Integer number) {
         Integer n = 1;
         for (int i = 1; i < number - 1; i++) {
---
+++ src/main/java/Services/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * this package is used for scanning data and showing it on the screen.
+ */
+package Services;
