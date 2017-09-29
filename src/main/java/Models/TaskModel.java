6f578cc60bb19006018587b6748450610860d799
MykolaShevchuk <shevchuk.mykola93@gmail.com>
2017-09-29 10:55:39 +0300

    comments & etc.

--- src/main/java/Models/TaskModel.java
+++ src/main/java/Models/TaskModel.java
@@ -2,6 +2,9 @@ package Models;
 
 import java.util.ArrayList;
 
+/**
+ * its model for all tasks.
+ */
 public abstract class TaskModel {
     public ArrayList<Integer> numberOperation(Integer naturalNumber) {
         return null;
