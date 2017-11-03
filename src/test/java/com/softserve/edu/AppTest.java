58ea6d4190474524c84fcb3c4b21336b65e56bd2
bebebe <hahaha123456@gmail.com>
2017-11-03 20:03:04 +0200

    Example Selenium. Template

--- src/test/java/com/softserve/edu/AppTest.java
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -1,15 +1,20 @@
 package com.softserve.edu;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-    
+
     @Test
     public void testApp() {
+        System.out.println("surefire.reports.directory = "
+                + System.getProperty("surefire.reports.directory"));
+        System.out.println("selenium-version = "
+                + System.getProperty("selenium-version"));
+        //
         Assert.assertTrue(true);
     }
 
