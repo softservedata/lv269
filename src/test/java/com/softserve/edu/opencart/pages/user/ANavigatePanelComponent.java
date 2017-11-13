fe8b3a22c8a928716294dead02954847f9eedbde
MykolaShevchuk <shevchuk.mykola93@gmail.com>
2017-11-13 22:16:20 +0200

    page

---
+++ src/test/java/com/softserve/edu/opencart/pages/user/ANavigatePanelComponent.java
@@ -0,0 +1,30 @@
+package com.softserve.edu.opencart.pages.user;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+abstract class ANavigatePanelComponent extends AHeaderComponent {
+
+    // Fields
+
+    private List<WebElement> pathElements;
+
+    public ANavigatePanelComponent(WebDriver driver) {
+        super(driver);
+    }
+
+    // PageObject
+
+    // get Data
+
+    // get Functional
+
+    // set Data
+
+    // set Functional
+
+    // Business Logic
+
+}
---
+++ src/test/java/com/softserve/edu/opencart/pages/user/HomePage.java
@@ -0,0 +1,54 @@
+package com.softserve.edu.opencart.pages.user;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends AHeaderComponent {
+
+    // Fields
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+        initProductComponents(By.cssSelector(".product-layout"));
+    }
+
+    // PageObject
+
+    // get Data
+
+    // get Functional
+
+    @Override
+    public List<String> getProductComponentTexts() {
+        return super.getProductComponentTexts();
+    }
+
+    @Override
+    public String getPriceTextByProductName(String productName) {
+        return super.getPriceTextByProductName(productName);
+    }
+
+    @Override
+    public double getPriceAmountByProductName(String productName) {
+        return super.getPriceAmountByProductName(productName);
+    }
+
+    // set Data
+
+    @Override
+    public void clickAddToCartByProductName(String productName) {
+        super.clickAddToCartByProductName(productName);
+    }
+
+    @Override
+    public void clickAddToWishByProductName(String productName) {
+        super.clickAddToWishByProductName(productName);
+    }
+
+    // set Functional
+
+    // Business Logic
+
+}
