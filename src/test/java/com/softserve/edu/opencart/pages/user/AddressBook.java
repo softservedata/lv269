caf22f886c076c9a3e2e5e7ca7f413ca94c90b86
myrekzvarych <mirekzvar@gmail.com>
2017-11-15 11:11:23 +0200

    add Address

--- src/test/java/com/softserve/edu/opencart/pages/user/AddressBook.java
+++ src/test/java/com/softserve/edu/opencart/pages/user/AddressBook.java
@@ -1,7 +1,69 @@
 package com.softserve.edu.opencart.pages.user;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 /**
  * Created by ${Mirek} .
  */
-public class AddressBook {
+public class AddressBookPage extends MyAccountPage {
+    private WebElement editButton;
+    private WebElement deleteButton;
+    private WebElement newAddressBook;
+    private WebElement backButton;
+
+    public AddressBookPage(WebDriver driver){
+        super(driver);
+        editButton = driver.findElement(By.cssSelector(".btn.btn-info"));
+        deleteButton = driver.findElement(By.cssSelector(".btn.btn-danger"));
+        newAddressBook = driver.findElement(By.cssSelector(".btn.btn-primary"));
+        backButton = driver.findElement(By.xpath(".//a[contains( text(),'Back')]"));
+    }
+    // PageObject
+
+    // get Data
+
+    public WebElement getEditButton() {
+        return editButton;
+    }
+
+    public WebElement getDeleteButton() {
+        return deleteButton;
+    }
+
+    public WebElement getNewAddressBook() {
+        return newAddressBook;
+    }
+
+    public WebElement getBackButton() {
+        return backButton;
+    }
+    // get Functional
+
+    // set Data
+    public void clickEditButton(){
+        getEditButton().click();
+    }
+
+    public void clickDeleteButton(){
+        getDeleteButton().click();
+    }
+
+    public void clickNewAddress(){
+        getNewAddressBook().click();
+    }
+
+    public void clickBackButton(){
+        getBackButton().click();
+    }
+    // set Functional
+
+    // Business Logic
+    public MyAccountPage gotoMyAccountPage(){
+        clickBackButton();
+        return new MyAccountPage(driver);
+    }
+
+    public
 }
---
+++ src/test/java/com/softserve/edu/opencart/pages/user/EditAddressPage.java
@@ -0,0 +1,13 @@
+package com.softserve.edu.opencart.pages.user;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by ${Mirek} on 15.11.2017.
+ */
+public class EditAddressPage extends MyAccountPage {
+
+    public EditAddressPage(WebDriver driver){
+        super(driver);
+    }
+}
