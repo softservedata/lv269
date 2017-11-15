f6b66d042d9e0eafb1aedcde0b7ca7c4afe47fb6
MykolaShevchuk <shevchuk.mykola93@gmail.com>
2017-11-15 22:17:59 +0200

    fix logout

--- src/test/java/com/softserve/edu/opencart/tests/smoke/LoginTest.java
+++ src/test/java/com/softserve/edu/opencart/tests/smoke/LoginTest.java
@@ -1,8 +1,6 @@
 package com.softserve.edu.opencart.tests.smoke;
 
 import com.softserve.edu.opencart.pages.user.HomePage;
-import com.softserve.edu.opencart.pages.user.LogoutPage;
-import com.softserve.edu.opencart.pages.user.MyAccountPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
@@ -23,10 +21,10 @@ public class LoginTest {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
          new HomePage(driver).gotoLoginPageFromMyAccountByPartialName()
-                .loginForLoginPageToUserPage("lv_269_test@ukr.net", "269lv269lv")
+                .loginForLoginPageMyAccountPage("lv_269_test@ukr.net", "269lv269lv")
                  .gotoLogoutPage().clickContinueButton();
 
-  
+
         driver.quit();
     }
 }
