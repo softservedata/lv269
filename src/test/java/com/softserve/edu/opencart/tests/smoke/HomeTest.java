35bb0b13a68a7f5439074f4aec3670925adfa44b
AKravtsov <andrjusyk@gmail.com>
2017-11-16 14:35:48 +0200

    minusShit

--- src/test/java/com/softserve/edu/opencart/tests/smoke/HomeTest.java
+++ src/test/java/com/softserve/edu/opencart/tests/smoke/HomeTest.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -32,6 +33,8 @@ public class HomeTest {
         System.setProperty("webdriver.chrome.driver",
                 "D:/eclipse/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+    	//System.setProperty("webdriver.gecko.driver", "D:/eclipse/geckodriver.exe");
+    	//WebDriver driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         //
         driver.get("http://server7.pp.ua");
