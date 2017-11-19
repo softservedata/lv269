25148ad028eea4cd0050d1dd9999d82decb9b595
yuliabeshlei <yulia.beshlei@gmail.com>
2017-11-19 19:45:42 +0200

    add new SpecialSymbols Test Case

---
+++ src/test/java/com/softserve/edu/opencart/tests/searchpage/SpecialSymbols.java
@@ -0,0 +1,43 @@
+package com.softserve.edu.opencart.tests.searchpage;
+
+import com.softserve.edu.opencart.data.applications.ApplicationSourceRepository;
+import com.softserve.edu.opencart.data.products.SearchProductRepository;
+import com.softserve.edu.opencart.pages.Application;
+import com.softserve.edu.opencart.pages.user.FailureSearchPage;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class SpecialSymbols {
+    @BeforeClass
+    public void beforeClass() {
+        Application.get(ApplicationSourceRepository.get().chromeServer7());
+    }
+
+    @AfterClass
+    public void afterClass() {
+        Application.remove();
+    }
+
+    @DataProvider
+    public Object[][] productData() {
+
+        return new Object[][]{
+                {"'"}, {"["}, {"!"}, {"$"}, {"%"}, {"^"}, {"&"}, {"+"},
+                {"|"}, {"~"}, {"="}, {"`"}, {"/"}, {":"}, {";"},{"?"},
+                {"SELECT * FROM PRODUCTS"}
+        };
+    }
+
+    @Test(dataProvider = "productData")
+    public void checkSearchProductResult(String specialSymbol){
+        FailureSearchPage searchPage = Application.get().loadHomePage()
+                .failureProductSearch(specialSymbol);
+        Assert.assertTrue(searchPage.nothingFound());
+    }
+
+}
