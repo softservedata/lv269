b9fd4c5d5169f4e2a0977302830ac0bb8a27ae25
MykolaShevchuk <shevchuk.mykola93@gmail.com>
2017-11-21 00:43:48 +0200

    fix  LoginTest add assert

---
+++ src/test/java/com/softserve/edu/opencart/tests/login/NegativeLoginTest.java
@@ -0,0 +1,43 @@
+package com.softserve.edu.opencart.tests.login;
+
+import com.softserve.edu.opencart.data.applications.ApplicationSourceRepository;
+import com.softserve.edu.opencart.data.users.IUser;
+import com.softserve.edu.opencart.data.users.UserRepository;
+import com.softserve.edu.opencart.pages.Application;
+import com.softserve.edu.opencart.pages.user.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.sql.SQLException;
+
+public class NegativeLoginTest {
+    @BeforeClass
+    public void beforeClass() throws SQLException {
+//        Application.get(ApplicationSourceRepository.get().chromeServer7());
+        Application.get(ApplicationSourceRepository.get().firefoxServer7());
+    }
+
+    @AfterClass
+    public void afterClass() {
+        Application.remove();
+    }
+
+    @DataProvider(name = "Authentication")
+    public static Object[][] credentials() {
+        return new Object[][]{{UserRepository.get().userUnknown()}};
+
+    }
+
+    @Test(dataProvider = "Authentication")
+    public void checkSuccessfulLogin(IUser user) throws SQLException {
+
+        LoginPage loginPage = Application.get().loadHomePage().gotoLoginPageFromMyAccount();
+        loginPage = loginPage.gotoLoginForLoginPageToWarning(user);
+        String actual = loginPage.getWarningDangerText();
+
+        Assert.assertEquals(actual, LoginPage.EXPECTED_FIRST_WARNING);
+    }
+}
