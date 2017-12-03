5a38789f98cbda8a4d60f1f88d1a015a4c8fe656
YuraIvanytsky <y.ivanytsky@gmail.com>
2017-12-03 19:36:44 +0200

    qwqqq

--- src/main/java/com/softserve/edu/App.java
+++ src/main/java/com/softserve/edu/App.java
@@ -4,18 +4,10 @@ package com.softserve.edu;
  * Hello world!
  *
  */
-public class App {
-
-	static final String TEXT = "Text %s is Text %s";
-
-	public static void main(String[] args) {
-		int a = 1;
-		int b= 2;
-		
-		System.out.printf(a +" "+ b + TEXT);
-		System.out.println("Hello World!");
-
-	}
-	
-	
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
 }
