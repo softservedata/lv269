8ba875ff5fae4e7e933d8612f533c9f66e563766
YuraIvanytsky <y.ivanytsky@gmail.com>
2017-12-05 20:17:47 +0200

    inser chaneges into testngCurrency.xml

---
+++ src/test/java/com/softserve/edu/opencart/tests/CurrencyTestRunner.java
@@ -0,0 +1,48 @@
+package com.softserve.edu.opencart.tests;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+
+import com.softserve.edu.opencart.data.applications.ApplicationSourceRepository;
+import com.softserve.edu.opencart.data.categories.CurrencyRepository;
+import com.softserve.edu.opencart.pages.Application;
+import com.softserve.edu.opencart.tools.ReporterWrapper;
+
+public abstract class CurrencyTestRunner {
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+    protected ReporterWrapper reporter;
+
+    // Use, if class Application is not singleton
+    // protected Application application;
+    
+    @BeforeSuite
+    public void beforeSuite() {
+    	logger.info("@BeforeClass for " + this.getClass().getName());
+    	Application.get(ApplicationSourceRepository.get().chromeImplicitServer7());
+    	reporter = Application.get().reporter();
+    }
+    
+    @BeforeClass
+    public void beforeClass() {
+        logger.info("@BeforeClass for " + this.getClass().getName());
+        Application.get().loadHomePage().chooseCurrencyByDetailCategory(CurrencyRepository.get().dollar());
+        reporter = Application.get().reporter();
+    }
+
+    @AfterClass
+    public void afterClass() {
+        logger.info("@AfterClass for " + this.getClass().getName());
+//        Application.remove();
+    }
+    
+    @AfterSuite
+    public void afterSuite() {
+    	logger.info("@AfterClass for " + this.getClass().getName());
+        Application.remove();
+    }
+
+}
