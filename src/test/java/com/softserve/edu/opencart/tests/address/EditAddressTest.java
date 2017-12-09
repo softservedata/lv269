df650b756c6dce8827916fb094f5bb0693fa19cf
myrekzvarych <mirekzvar@gmail.com>
2017-12-09 16:28:49 +0200

    add log and reporter

--- src/test/java/com/softserve/edu/opencart/tests/address/EditAddressTest.java
+++ src/test/java/com/softserve/edu/opencart/tests/address/EditAddressTest.java
@@ -1,40 +1,19 @@
 package com.softserve.edu.opencart.tests.address;
 
 import com.softserve.edu.opencart.data.applications.ApplicationSourceRepository;
-import com.softserve.edu.opencart.data.field.IListOfFields;
 import com.softserve.edu.opencart.data.field.ListOfFieldsRepository;
-import com.softserve.edu.opencart.data.products.ProductRepository;
 import com.softserve.edu.opencart.data.users.IUser;
-import com.softserve.edu.opencart.data.users.User;
-import com.softserve.edu.opencart.data.users.UserRepository;
-import com.softserve.edu.opencart.pages.AlertsText;
 import com.softserve.edu.opencart.pages.Application;
-import com.softserve.edu.opencart.pages.GeneralLocators;
-import com.softserve.edu.opencart.pages.user.AddressBookPage;
 import com.softserve.edu.opencart.pages.user.EditAddressPage;
+import com.softserve.edu.opencart.tests.TestRunner;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
-public class EditAddressTest {
-    @BeforeClass
-    public void beforClass() {
-        //Application.get(ApplicationSourceRepository.get().chromeImplicitServer7());
-        Application.get(ApplicationSourceRepository.get().firefoxImplicitServer7());
-    }
+public class EditAddressTest extends TestRunner {
 
-    @AfterClass
-    public void afterClass() {
-        Application.remove();
-    }
-
-    @BeforeMethod
-    public void beforMethod() {
-    }
 
     @AfterMethod
     public void afterMethod() {
@@ -50,6 +29,9 @@ public class EditAddressTest {
 
     @Test(dataProvider = "editAddress")
     public void checkEmptyField(IUser user, IUser wrongField, String expected) {
+        logger.info(String.format("check Empty Field:  of User: %s",wrongField.getEmail()));
+        reporter.info(String.format("check Empty Address:  of User: %s", wrongField.getEmail()));
+
         EditAddressPage editAddressPage = Application.get().loadHomePage().gotoLoginPageFromMyAccount()
                 .gotoLoginForLoginPageToMyAccountPage(user)
                 .gotoAddressBookPageRightColumn().modifyAddressBookData(user);
@@ -59,33 +41,6 @@ public class EditAddressTest {
         String actual = editAddressPage.getWarningDangerTextforField();
 
         Assert.assertEquals(actual, expected);
-
     }
 
-
-
-
-  /*  @DataProvider(name = "editAddress")
-    public Object[][] userData() {
-        return new Object[][] {
-                {UserRepository.get().userZvarych(), GeneralLocators.FIRST_NAME_LOCATOR, AlertsText.FIRST_NAME_MUST_BE_1_TO_32.toString()},
-                {UserRepository.get().userZvarych(), GeneralLocators.LAST_NAME_LOCATOR,AlertsText.LAST_NAME_MUST_BE_1_TO_32.toString()},
-                {UserRepository.get().userZvarych(), GeneralLocators.ADDRESS_LOCATOR, AlertsText.ADDRESS_MUST_BE_3_TO_128.toString()},
-                {UserRepository.get().userZvarych(), GeneralLocators.CITY_LOCATOR, AlertsText.CITY_MUST_BE_2_TO_128.toString()}
-        };
-
-    }
-
-
-    @Test(dataProvider = "editAddress")
-    public void checkEmptyField(IUser user, GeneralLocators locator, String expected){
-        EditAddressPage editAddressPage = Application.get().loadHomePage().gotoLoginPageFromMyAccount()
-                .gotoLoginForLoginPageToMyAccountPage(user)
-                .gotoAddressBookPageRightColumn().modifyAddressBookData(user);
-        editAddressPage.clearField(editAddressPage.findField(locator));
-        editAddressPage.saveChangesAddress();
-        String acttual = editAddressPage.getWarningDangerTextforField();
-        Assert.assertEquals(acttual, expected);
-
-    }*/
 }
